b684bbecc53d3562190db1a279a13b0288260d1b
Evgeny <jevgenijs240693@gmail.com>
2021-08-29 06:27:07 +0100

    Added menager

--- djangoReact/mainApp/admin.py
+++ djangoReact/mainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BlogPost, BlogCategory
+
+
+admin.site.register(BlogPost)
+admin.site.register(BlogCategory)
 
 # Register your models here.
--- djangoReact/mainApp/models.py
+++ djangoReact/mainApp/models.py
@@ -8,14 +8,34 @@ class BlogCategory(models.Model):
     def __str__(self):
         return self.name
 
+
+class BlogPostQuerySet(models.QuerySet):
+
+    def find_by_title_in_qs(self, post_title):
+        return self.filter(title__icontains=post_title)
+
+
+
+class BlogPostManager(models.Manager):
+    def get_queryset(self):
+        return BlogPostQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().filter(in_archive=False)
+
+    def find_by_title_in_qs(self, post_title):
+        return self.get_queryset().find_by_title_in_qs(post_title)
+
+
 class BlogPost(models.Model):
     blog_category = models.ForeignKey(BlogCategory, verbose_name='Category name', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Post name')
     content = models.TextField()
-    image = models.ImageField(upload_to='blog_posts/')
+    image = models.ImageField(upload_to='blog_posts/', blank=True, null=True)
     published = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True)
     in_archive = models.NullBooleanField(default=False)
+    objects = BlogPostManager()
 
     def __str__(self):
         return f"{self.title} from \"{self.blog_category.name}\" category"
